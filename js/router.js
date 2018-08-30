99ad4bd2b7253b21516ac9d885b40461d83b7927
wangxp <1533266567@qq.com>
2018-08-30 17:21:03 +0800

    基本完成

---
+++ js/router.js
@@ -0,0 +1,41 @@
+(function() {
+	var route = [];
+	var aboutRoute = ['introduc','advantage','group','dynamic','contact'];
+	var serverRoute = ['details','capital','clause','account','download','privacy','deposit','dispute','cookies'];
+	var collegeRoute = ['teaching','skill','data','discount'];
+	var infoRoute = ['hot','special','expert','calendar'];
+
+	var path = window.location.pathname.split('/');
+	var pathName = path[path.length-1].split('.')[0];
+
+	switch(pathName) {
+		case 'aboutus':
+			route = aboutRoute;
+		break;
+		case 'product':
+			route = serverRoute;
+		break;
+		case 'data':
+			route = collegeRoute;
+		break;
+		case 'information':
+			route = infoRoute;
+		break;
+		default:
+			return;
+			break;
+	};
+
+	var type = window.location.search.split('=')[1];
+	var index = 0
+
+	for( var i = 0;i < route.length; i++ ) {
+		if( route[i] === type ) {
+			index = i;
+		}
+	}
+
+	$('.colum-lists li').eq(index).addClass('active').siblings('li').removeClass('active');
+	$('.content').eq(index).removeClass('none').siblings('.content').addClass('none');
+
+})()
